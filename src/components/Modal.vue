c75f3026e733d029a58327a3293cda60586a8c2d
Molina-Q <quentinmolina1@gmail.com>
2024-07-02 12:03:02 +0200

    feat: Improve modal component functionality and styling

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,10 +1,24 @@
 <template>
-    <div class="backdrop">
-        <div class="modal">
-            <p>modal content</p>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{ sale: theme === 'sale' }">
+            <slot>No content</slot>
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>
         </div>
     </div>
 </template>
+
+<script>
+    export default {
+        props: ['theme'],
+        methods: {
+            closeModal() {
+                this.$emit('close')
+            }
+        }
+    }
+</script>
 
 <style>
     .backdrop {
@@ -21,5 +35,29 @@
         margin: 100px auto;
         background: white;
         border-radius: 10px;
+    }
+    .modal h1 {
+        color: rgb(12, 211, 168);
+        border: none;
+        padding: 0;
+    }
+    .modal.sale {
+        background: crimson;
+        color: white;
+    }
+    .modal.sale h1 {
+        color: white;
+    }
+    .modal .actions {
+        text-align: center;
+        margin: 30px 0 10px 0;
+    }
+    .modal .actions a {
+        color: #333;
+        padding: 8px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        text-decoration: none;
+        margin: 10px;
     }
 </style>
